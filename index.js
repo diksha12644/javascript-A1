5f6870f4f40939971b5caf1a948e79e393baf7ab
Diksha samyal <dixasamyal1998@gmail.com>
2024-10-21 19:04:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+// let a = 5
+// let b = 2
+// let res = (a+b);
+// console.log (res);
+
+//2.
+// let A = 5;
+// let B = 3;
+
+// function Is_Valid(A, B) {
+ 
+//     return (A < 10 && B < 10) && (A > B);
+// }
+
+// console.log(Is_Valid(A, B)); 
+
+
+
+//3.
+// let A = 12;
+// let B = 20;
+
+// function Check(A, B) {
+   
+//     if (A % 10 === 0 && B % 10 === 0) {
+//         return true;
+//     }
+
+//     else if (A % 10 !== 0 && B % 10 !== 0) {
+//         return false;
+//     }
+    
+//     else {
+//         return true;
+//     }
+// }
+// console.log(Check(A, B)); 
+
+
+
+//4.
+
+
+// function First_Digit(N) {
+//     return parseInt(N.toString()[0]);
+// }
+
+
+// let N = 4567;
+
+
+// console.log(First_Digit(N)); 
+
+
+
+
+//5
+// function Last_Digit(a) {
+//     const lastDigit = a % 10; 
+//     console.log(lastDigit); 
+// }
+
+
+// Last_Digit(4567); 
+
+
+//6.
+// let A = 2;
+// let B = 9;
+// function Find_the_remainder(A, B) {
+
+//     return B % A;
+// }
+
+// console.log(Find_the_remainder(A, B));
+
+
+//7.
+
+// let a= 2;
+// let b= 5;
+//  b *=a
+
+// console.log(b);
+
+
+// //8.
+
+// function res(A, B, C) {
+//     var total = A + B + C;
+//     var average = (total / 3).toFixed(2);
+//     var data = "";
+//     data = {
+//         "total":total,
+//         "average": average
+//     }
+//     return(data);
+// }
+
+// var A = 50;
+// var B = 20;
+// var C = 100;
+
+// var result = res(A, B, C);
+
+// console.log("Total Marks:", result.total ,result.average);
+// //console.log("Average Marks:", result.average);
